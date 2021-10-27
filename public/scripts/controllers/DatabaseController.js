5ab13f6c65a80070c02b034abbed025c54bab3ee
ericknathan <erick.capito@hotmail.com>
2021-10-27 11:09:16 -0300

    🗃️ Database Controller finished

--- public/scripts/controllers/DatabaseController.js
+++ public/scripts/controllers/DatabaseController.js
@@ -1,29 +1,45 @@
-const Database = {
-  user: {
-    create: (name) => {},
-    getAll: () => {},
-    update: (name, type, mode = '') => {},
-    exists: (name) => {}
-  }
-}
-
-/* MODELO DOS DADOS:
-  [
-    {
-      "name": "",
-      "wins": {
-        "pvp": 0,
-        "easy": 0,
-        "medium": 0,
-        "hard": 0
-      },
-      "losses": {
-        "pvp": 0,
-        "easy": 0,
-        "medium": 0,
-        "hard": 0
-      },
-      "draws": 0
+class Database {
+    create(name) {
+      if(!this.exists(name)) {
+        localStorage.setItem(name, JSON.stringify({
+          "name": name,
+          "wins": {
+            "pvp": 0,
+            "easy": 0,
+            "medium": 0,
+            "hard": 0
+          },
+          "losses": {
+            "pvp": 0,
+            "easy": 0,
+            "medium": 0,
+            "hard": 0
+          },
+          "draws": 0
+        }));
+      }
+      return this.get(name);
     }
-  ]
-*/
+
+    get(name) {
+      return JSON.parse(localStorage.getItem(name));
+    }
+
+    getAll() {
+      return { ...localStorage };
+    }
+
+    update(name, type, mode = '') {
+      const data = this.get(name);
+
+      if(mode !== '') data[type][mode] += 1;
+      else data[type] += 1;
+
+      localStorage.setItem(name, JSON.stringify(data));
+    }
+    
+    exists(name) {
+      if(this.get(name)) return true;
+      return false;
+    }
+}
